fa566cf55d6deacb2ddabaebeb5107790c3cd876
Adrian Sochaniwsky <6884645+adrian-soch@users.noreply.github.com>
2020-07-01 14:05:51 -0400

    Add files via upload

---
+++ arduino/ros_servo_sub.ino
@@ -0,0 +1,46 @@
+/*
+ * Using Base code from: rosserial Servo Control Example
+ * Using Laser module instead of built in LED
+ * Swapped move_servo message type from Uint16 -> Uint8
+ */
+
+#if (ARDUINO >= 100)
+ #include <Arduino.h>
+#else
+ #include <WProgram.h>
+#endif
+
+#include <Servo.h> 
+#include <ros.h>
+#include <std_msgs/UInt8.h> // 0 - 255 values accepted
+
+int laserPin = 8;
+
+ros::NodeHandle  nh;
+
+Servo servo;
+
+void servo_cb( const std_msgs::UInt8& cmd_msg){
+  servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
+  if(cmd_msg.data == 90)
+    digitalWrite(laserPin, LOW);  //laser off when stationary 
+   else
+    digitalWrite(laserPin, HIGH);  //laser on when moving 
+}
+
+
+ros::Subscriber<std_msgs::UInt8> sub("servo", servo_cb);
+
+void setup(){
+  pinMode(laserPin, OUTPUT);
+
+  nh.initNode();
+  nh.subscribe(sub);
+  
+  servo.attach(9); //use pin 9 to control servo
+}
+
+void loop(){
+  nh.spinOnce();
+  delay(2);
+}

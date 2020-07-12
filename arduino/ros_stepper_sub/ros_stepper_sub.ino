2d89c3b5caca47bef85f8b1945b7bdffff04ba76
Adrian <adr.soch@outlook.com>
2020-07-12 17:36:05 -0400

    small changes

--- arduino/ros_stepper_sub/ros_stepper_sub.ino
+++ arduino/ros_stepper_sub/ros_stepper_sub.ino
@@ -1,6 +1,6 @@
 
 /*
- * 
+ * Author: Adrian Sochaniwsky
  */
  
 #if (ARDUINO >= 100)
@@ -11,7 +11,7 @@
 
 #include <AccelStepper.h>
 #include <ros.h>
-#include <std_msgs/UInt8.h> // 0 - 255 values accepted
+#include <std_msgs/UInt8.h>
 
 int laserPin = 8;
 
@@ -20,12 +20,11 @@ ros::NodeHandle  nh;
 
 void stepper_cb( const std_msgs::UInt8 & cmd_msg){
   servo.write(cmd_msg.data); //set servo angle, should be from 0-180  
-  if(cmd_msg.data == 90)
+  if(cmd_msg.data != 0)
     digitalWrite(laserPin, LOW);  //laser off when stationary 
    else
     digitalWrite(laserPin, HIGH);  //laser on when moving 
 }
-
 
 ros::Subscriber<std_msgs::UInt8> sub("stepper", stepper_cb);
 
@@ -34,7 +33,7 @@ void setup()
   pinMode(laserPin, OUTPUT);
   
   stepper.setMaxSpeed(1000);
-  stepper.setSpeed(50);	
+  //stepper.setSpeed(50);	
   
   nh.initNode();
   nh.subscribe(sub);
